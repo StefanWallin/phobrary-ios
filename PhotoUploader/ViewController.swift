b9f57a9842715e996eea47ae0996349e302d55c8
Stefan Wallin <stefan.w@festiz.com>
2018-12-27 09:29:51 +0100

    Initial Commit

---
+++ PhotoUploader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoUploader
+//
+//  Created by Stefan Wallin on 2018-12-27.
+//  Copyright © 2018 Stefan Wallin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
